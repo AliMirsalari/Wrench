ddfe2fcaa7eaa864555b7c8797682ecf3043d53d
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-16 16:27:20 +0330

    Add WriteImageException.java

---
+++ src/main/java/com/ali/mirsalari/wrench/exception/WriteImageException.java
@@ -0,0 +1,7 @@
+package com.ali.mirsalari.wrench.exception;
+
+public class WriteImageException extends RuntimeException{
+    public WriteImageException(Throwable cause) {
+        super(cause);
+    }
+}
