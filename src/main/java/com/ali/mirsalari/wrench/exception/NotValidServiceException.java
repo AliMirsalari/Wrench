93b961355843250bdd5506e0bb5b1f1bbb20e5f8
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-16 16:27:10 +0330

    Add NotValidServiceException.java

---
+++ src/main/java/com/ali/mirsalari/wrench/exception/NotValidServiceException.java
@@ -0,0 +1,7 @@
+package com.ali.mirsalari.wrench.exception;
+
+public class NotValidServiceException extends RuntimeException{
+    public NotValidServiceException(String message) {
+        super(message);
+    }
+}
