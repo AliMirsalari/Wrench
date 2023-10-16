3366837b12e125604dcaf45a6a2f8001c0769003
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-16 16:22:20 +0330

    Update AdminServiceImpl.java

--- src/main/java/com/ali/mirsalari/wrench/service/impl/AdminServiceImpl.java
+++ src/main/java/com/ali/mirsalari/wrench/service/impl/AdminServiceImpl.java
@@ -1,14 +1,13 @@
 package com.ali.mirsalari.wrench.service.impl;
 
 import com.ali.mirsalari.wrench.entity.Admin;
-import com.ali.mirsalari.wrench.exception.EmailExistException;
+import com.ali.mirsalari.wrench.exception.DuplicateEmailException;
 import com.ali.mirsalari.wrench.exception.NotFoundException;
 import com.ali.mirsalari.wrench.exception.NotValidEmailException;
 import com.ali.mirsalari.wrench.exception.NotValidPasswordException;
 import com.ali.mirsalari.wrench.repository.AdminRepository;
 import com.ali.mirsalari.wrench.service.AdminService;
 import com.ali.mirsalari.wrench.util.Validator;
-import jakarta.persistence.EntityTransaction;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
@@ -23,31 +22,15 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     @Transactional
-    public Admin save(Admin admin) throws EmailExistException, NotValidPasswordException, NotValidEmailException {
-        if (findByEmail(admin.getEmail()).isPresent()) {
-            throw new EmailExistException("Email already exists");
-        }
-        if (!Validator.isValidPassword(admin.getPassword())) {
-            throw new NotValidPasswordException("Password is not good!");
-        }
-        if (!Validator.isValidEmail(admin.getEmail())) {
-            throw new NotValidEmailException("Email is not good!");
-        }
+    public Admin save(Admin admin){
+        validation(admin);
         return adminRepository.save(admin);
     }
 
     @Override
     @Transactional
-    public Admin update(Admin admin) throws NotFoundException, NotValidPasswordException, NotValidEmailException {
-        if (admin.getId() == null || adminRepository.findById(admin.getId()) == null) {
-            throw new NotFoundException("Admin with id: " + admin.getId() + " is not found.");
-        }
-        if (!Validator.isValidPassword(admin.getPassword())) {
-            throw new NotValidPasswordException("Password is not valid!");
-        }
-        if (!Validator.isValidEmail(admin.getEmail())) {
-            throw new NotValidEmailException("Email is not good!");
-        }
+    public Admin update(Admin admin) {
+        validation(admin);
         return adminRepository.save(admin);
     }
 
@@ -65,20 +48,21 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     @Transactional
+    public Optional<Admin> findByEmail(String email) {
+        return adminRepository.findAdminByEmail(email);
+    }
+
+
+    @Override
+    @Transactional
     public List<Admin> findAll() {
         return adminRepository.findAll();
     }
 
     @Override
     @Transactional
-    public Optional<Admin> findByEmail(String email) {
-        return adminRepository.findAdminByEmail(email);
-    }
-
-    @Override
-    @Transactional
-    public Admin changePassword(String newPassword, Admin admin) throws NotFoundException, NotValidPasswordException, NotValidEmailException {
-        if (admin.getId() == null || adminRepository.findById(admin.getId()) == null) {
+    public Admin changePassword(String newPassword, Admin admin){
+        if (admin.getId() == null || adminRepository.findById(admin.getId()).isEmpty()) {
             throw new NotFoundException("Admin with id: " + admin.getId() + " is not found.");
         }
         if (!Validator.isValidPassword(newPassword)) {
@@ -86,5 +70,19 @@ public class AdminServiceImpl implements AdminService {
         }
         admin.setPassword(newPassword);
         return update(admin);
+    }
+
+    @Override
+    @Transactional
+    public void validation(Admin admin){
+        if (findByEmail(admin.getEmail()).isPresent()) {
+            throw new DuplicateEmailException("Email already exists");
+        }
+        if (!Validator.isValidPassword(admin.getPassword())) {
+            throw new NotValidPasswordException("Password is not good!");
+        }
+        if (!Validator.isValidEmail(admin.getEmail())) {
+            throw new NotValidEmailException("Email is not good!");
+        }
     }
 }
