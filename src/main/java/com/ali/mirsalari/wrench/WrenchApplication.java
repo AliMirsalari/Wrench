0dc7f39fec5b5972529487b1c0882b730743a1da
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-16 16:26:00 +0330

    Update WrenchApplication.java

--- src/main/java/com/ali/mirsalari/wrench/WrenchApplication.java
+++ src/main/java/com/ali/mirsalari/wrench/WrenchApplication.java
@@ -1,24 +1,41 @@
 package com.ali.mirsalari.wrench;
 
 import com.ali.mirsalari.wrench.config.ServiceConfig;
+import com.ali.mirsalari.wrench.entity.Expert;
+import com.ali.mirsalari.wrench.service.ExpertService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
+import java.time.Instant;
+
 @SpringBootApplication
 public class WrenchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WrenchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WrenchApplication.class, args);
+    }
 
-	@Bean
-	CommandLineRunner commandLineRunner (ServiceConfig serviceConfig){
-		return args -> {
-			serviceConfig.start();
-		};
-	}
+    @Bean
+    CommandLineRunner commandLineRunner(ServiceConfig serviceConfig,
+                                        ExpertService expertService
+                                        ) {
+        return args -> {
+//			serviceConfig.start();
+//            Expert expert = new Expert(
+//                    1L,
+//                    "Ali",
+//                    "Mirsalari",
+//                    "Ali@gmail.com",
+//                    "Fr3$#B1gD0g$RanTh3H1ll$F@st!",
+//                    Instant.now(),
+//                    "src/main/resources/images/Avatar.jpg");
+//            expertService.save(expert);
+
+//            expertService.retrieveAndSavePhotoToFile(expertService.findById(1L).get(), "src/main/resources/images/new/Avatar.jpg");
+        };
+    }
 
 }
