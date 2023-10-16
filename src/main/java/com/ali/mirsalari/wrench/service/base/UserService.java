2ba10e5a682be6cccc771d8aedeeb7a093c53cae
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-16 16:25:34 +0330

    Update UserService.java

--- src/main/java/com/ali/mirsalari/wrench/service/base/UserService.java
+++ src/main/java/com/ali/mirsalari/wrench/service/base/UserService.java
@@ -1,5 +1,6 @@
 package com.ali.mirsalari.wrench.service.base;
 
+import com.ali.mirsalari.wrench.entity.Admin;
 import com.ali.mirsalari.wrench.exception.NotFoundException;
 import com.ali.mirsalari.wrench.exception.NotValidEmailException;
 import com.ali.mirsalari.wrench.exception.NotValidPasswordException;
@@ -9,5 +10,6 @@ import java.util.Optional;
 public interface UserService<T, EMAIL>{
     Optional<T> findByEmail (EMAIL email);
     T changePassword(String newPassword, T t) throws NotFoundException, NotValidPasswordException, NotValidEmailException;
+    void validation(T t);
 
 }
