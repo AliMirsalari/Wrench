8e3b2b20a0fca3cadde93f92ad7a70282baddac8
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-17 19:33:09 +0330

    Update DuplicateException.java

--- src/main/java/com/ali/mirsalari/wrench/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.ali.mirsalari.wrench.exception;
-
-public class DuplicateEmailException extends RuntimeException{
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-}
