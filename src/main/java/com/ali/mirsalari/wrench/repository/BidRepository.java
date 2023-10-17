b5b4c541b801204981acbeb3acf30976a6883870
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-17 19:31:28 +0330

    Update BidRepository.java

--- src/main/java/com/ali/mirsalari/wrench/repository/BidRepository.java
+++ src/main/java/com/ali/mirsalari/wrench/repository/BidRepository.java
@@ -1,10 +1,35 @@
 package com.ali.mirsalari.wrench.repository;
 
 import com.ali.mirsalari.wrench.entity.Bid;
+import com.ali.mirsalari.wrench.entity.Order;
+import com.ali.mirsalari.wrench.entity.Service;
+import com.ali.mirsalari.wrench.entity.enumeration.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface BidRepository
         extends JpaRepository<Bid,Long> {
+
+    @Query("SELECT b FROM Bid b WHERE b.order = :order ORDER BY b.suggestedPrice ASC")
+    List<Bid> findRelatedBidsOrderByPriceAsc(@Param("order") Order order);
+    @Query("SELECT b FROM Bid b WHERE b.order = :order ORDER BY b.suggestedPrice DESC")
+    List<Bid> findRelatedBidsOrderByPriceDesc(@Param("order") Order order);
+
+    @Query("SELECT b FROM Bid b WHERE b.order = :order ORDER BY b.expert.score ASC")
+    List<Bid> findRelatedBidsOrderByScoreAsc(@Param("order") Order order);
+    @Query("SELECT b FROM Bid b WHERE b.order = :order ORDER BY b.expert.score DESC")
+    List<Bid> findRelatedBidsOrderByScoreDesc(@Param("order") Order order);
+
+    @Query("SELECT b FROM Bid b WHERE b.order = :order AND b.selectionDate IS NOT NULL")
+    Optional<Bid> findSelectedBid(@Param("order") Order order);
+
+
 }
