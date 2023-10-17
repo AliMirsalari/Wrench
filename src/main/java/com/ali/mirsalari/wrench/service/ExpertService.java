be6bea6174f47322c980c7fea7816be122c616f3
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-17 19:33:34 +0330

    Update ExpertService.java

--- src/main/java/com/ali/mirsalari/wrench/service/ExpertService.java
+++ src/main/java/com/ali/mirsalari/wrench/service/ExpertService.java
@@ -1,21 +1,25 @@
 package com.ali.mirsalari.wrench.service;
 
 import com.ali.mirsalari.wrench.entity.Expert;
-import com.ali.mirsalari.wrench.exception.DuplicateEmailException;
+import com.ali.mirsalari.wrench.exception.DuplicateException;
 import com.ali.mirsalari.wrench.exception.NotFoundException;
 import com.ali.mirsalari.wrench.exception.NotValidEmailException;
 import com.ali.mirsalari.wrench.exception.NotValidPasswordException;
 import com.ali.mirsalari.wrench.service.base.CrudService;
 import com.ali.mirsalari.wrench.service.base.UserService;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ExpertService
         extends CrudService<Expert, Long>,
         UserService<Expert, String> {
-    Expert approveExpert(Long id) throws NotFoundException, NotValidPasswordException, DuplicateEmailException, NotValidEmailException;
-    Expert addSkill(Long skillId, Expert expert) throws NotFoundException, NotValidPasswordException, DuplicateEmailException, NotValidEmailException;
-    Expert removeSkill(Long skillId, Expert expert) throws NotFoundException, NotValidPasswordException, DuplicateEmailException, NotValidEmailException;
-
+    @Transactional
+    Expert approveExpert(Long id);
+    @Transactional
+    Expert addSkill(Long skillId, Expert expert);
+    @Transactional
+    Expert removeSkill(Long skillId, Expert expert);
+    @Transactional
     void retrieveAndSavePhotoToFile(Expert expert, String filePath);
 }
