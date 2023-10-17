fe6771e0bf3cdda0be7ed145f2ba5a831a05b13b
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-17 19:35:36 +0330

    Update ServiceService.java

--- src/main/java/com/ali/mirsalari/wrench/service/ServiceService.java
+++ src/main/java/com/ali/mirsalari/wrench/service/ServiceService.java
@@ -4,6 +4,7 @@ import com.ali.mirsalari.wrench.entity.Service;
 import com.ali.mirsalari.wrench.exception.NotFoundException;
 import com.ali.mirsalari.wrench.exception.ServiceExistException;
 import com.ali.mirsalari.wrench.service.base.CrudService;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,8 +12,11 @@ import java.util.Optional;
 @org.springframework.stereotype.Service
 public interface ServiceService
         extends CrudService<Service, Long> {
+    @Transactional
     List<Service> findAllServices ();
+    @Transactional
     List<Service> findAllSubServices ();
+    @Transactional
     Optional<Service> findByName(String name);
 
 
