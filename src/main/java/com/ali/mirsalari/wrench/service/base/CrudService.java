1206e61d64a94d28b101ca41de2e82294f3cc6b3
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-17 19:32:33 +0330

    Update CrudService.java

--- src/main/java/com/ali/mirsalari/wrench/service/base/CrudService.java
+++ src/main/java/com/ali/mirsalari/wrench/service/base/CrudService.java
@@ -1,14 +1,23 @@
 package com.ali.mirsalari.wrench.service.base;
 
+import com.ali.mirsalari.wrench.entity.Order;
 import com.ali.mirsalari.wrench.exception.*;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CrudService<T, ID> {
+    @Transactional
     T save (T t);
+    @Transactional
     T update (T t);
+    @Transactional
+    T uncheckedUpdate(T t);
+    @Transactional
     void remove (ID id);
+    @Transactional
     Optional<T> findById (ID id);
+    @Transactional
     List<T> findAll ();
 }
