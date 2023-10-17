bfeec64152030f2171130b867ca8a2ad8b0fee02
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-17 19:35:07 +0330

    Update OrderService.java

--- src/main/java/com/ali/mirsalari/wrench/service/OrderService.java
+++ src/main/java/com/ali/mirsalari/wrench/service/OrderService.java
@@ -1,10 +1,24 @@
 package com.ali.mirsalari.wrench.service;
 
+import com.ali.mirsalari.wrench.entity.Bid;
+import com.ali.mirsalari.wrench.entity.Expert;
 import com.ali.mirsalari.wrench.entity.Order;
 import com.ali.mirsalari.wrench.service.base.CrudService;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface OrderService
         extends CrudService<Order, Long> {
+
+    @Transactional
+    List<Order> findRelatedOrders(Expert expert);
+
+    @Transactional
+    void changeOrderStatusToStarted(Order order, Bid bid);
+
+    @Transactional
+    void changeOrderStatusToDone(Order order);
 }
