c3d7351544ef2ea190e5455dceada1c71d5f4631
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-18 14:34:42 +0330

    Update OrderServiceImplTest.java

--- src/test/java/com/ali/mirsalari/wrench/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/ali/mirsalari/wrench/service/impl/OrderServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.ali.mirsalari.wrench.service.impl;
 
+import com.ali.mirsalari.wrench.entity.Bid;
+import com.ali.mirsalari.wrench.entity.Expert;
 import com.ali.mirsalari.wrench.entity.Order;
 import com.ali.mirsalari.wrench.entity.Service;
 import com.ali.mirsalari.wrench.entity.enumeration.OrderStatus;
@@ -15,9 +17,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -32,7 +38,9 @@ class OrderServiceImplTest {
     private Order order;
     private Service it;
     private Service computer;
-
+    private Expert expert;
+    private Service service;
+    private Bid bid;
     @BeforeEach
     void setUp() {
         order = new Order(
@@ -50,6 +58,36 @@ class OrderServiceImplTest {
                 it,
                 null,
                 null);
+        expert = new Expert(
+                "Hashem",
+                "Momen",
+                "hashem@gmail.com",
+                "4s6]5rf4FSDF#%#",
+                0L,
+                Instant.now(),
+                0,
+                new HashSet<>(),
+                null,
+                null
+        );
+        service = new Service(7L,
+                "IT",
+                1_000_000L,
+                "IT services",
+                null,
+                null,
+                null);
+        Set<Service> services = expert.getSkills();
+        services.add(service);
+        expert.setSkills(services);
+        bid = new Bid(
+                Instant.now(),
+                9_000_000L,
+                Instant.now().plus(Duration.ofDays(2)),
+                Instant.now().plus(Duration.ofDays(10)),
+                expert,
+                order
+        );
     }
 
 
@@ -164,5 +202,72 @@ class OrderServiceImplTest {
         List<Order> orders = underTest.findAll();
         //Assert
         assertNotNull(orders);
+    }
+    @Test
+    void itShouldFindRelatedOrders() {
+        //Arrange
+        when(orderRepository.findRelatedOrders(any(),any())).thenReturn(List.of(order,order,order));
+        //Act
+        List<Order> orders = underTest.findRelatedOrders(expert);
+        //Assert
+        assertNotNull(orders);
+    }
+    @Test
+    void itShouldUpdateOrderWithoutChecking() {
+        //Arrange
+        when(orderRepository.save(any())).thenReturn(order);
+        //Act
+        Order tempOrder = underTest.uncheckedUpdate(order);
+        //Assert
+        assertNotNull(tempOrder);
+    }
+
+    @Test
+    void itShouldThrowIllegalStateExceptionWhenOrderStatusIsWrong() {
+        //Arrange
+        order.setOrderStatus(OrderStatus.DONE);
+        //Act and Assert
+        assertThrows(IllegalStateException.class, () -> {
+            underTest.changeOrderStatusToStarted(order,bid);
+        });
+    }
+    @Test
+    void itShouldThrowNotValidTimeException() {
+        //Arrange
+        order.setOrderStatus(OrderStatus.WAITING_FOR_THE_EXPERT_TO_COME_TO_YOUR_PLACE);
+        bid.setStartTime(Instant.now().plus(1, ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS).plus(9, ChronoUnit.HOURS));
+        //Act and Assert
+        assertThrows(NotValidTimeException.class, () -> {
+            underTest.changeOrderStatusToStarted(order,bid);
+        });
+    }
+
+    @Test
+    void itShouldChangeOrderStatusToStarted() {
+        //Arrange
+        order.setOrderStatus(OrderStatus.WAITING_FOR_THE_EXPERT_TO_COME_TO_YOUR_PLACE);
+        bid.setStartTime(Instant.now().minus(1, ChronoUnit.DAYS).truncatedTo(ChronoUnit.DAYS).plus(9, ChronoUnit.HOURS));
+        //Act
+        underTest.changeOrderStatusToStarted(order,bid);
+        //Assert
+        assertEquals(order.getOrderStatus(), OrderStatus.STARTED);
+    }
+    @Test
+    void itShouldThrowIllegalStateExceptionWhenOrderStatusIsNotStarted() {
+        //Arrange
+        order.setOrderStatus(OrderStatus.DONE);
+        //Act and Assert
+        assertThrows(IllegalStateException.class, () -> {
+            underTest.changeOrderStatusToDone(order);
+        });
+    }
+    @Test
+    void itShouldChangeOrderStatusToDone() {
+        //Arrange
+        order.setOrderStatus(OrderStatus.STARTED);
+        //Act
+        underTest.changeOrderStatusToDone(order);
+        //Assert
+        assertEquals(order.getOrderStatus(), OrderStatus.DONE);
     }
 }
