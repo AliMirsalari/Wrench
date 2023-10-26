e27d9ae10ac4ca244688e4ab6689270c6ca91b6e
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-27 00:01:30 +0330

    Update WrenchApplication.java

--- src/main/java/com/ali/mirsalari/wrench/WrenchApplication.java
+++ src/main/java/com/ali/mirsalari/wrench/WrenchApplication.java
@@ -1,41 +1,28 @@
 package com.ali.mirsalari.wrench;
 
 import com.ali.mirsalari.wrench.config.ServiceConfig;
+import com.ali.mirsalari.wrench.entity.Bid;
 import com.ali.mirsalari.wrench.entity.Expert;
+import com.ali.mirsalari.wrench.entity.Order;
+import com.ali.mirsalari.wrench.entity.Service;
+import com.ali.mirsalari.wrench.service.BidService;
 import com.ali.mirsalari.wrench.service.ExpertService;
+import com.ali.mirsalari.wrench.service.OrderService;
+import com.ali.mirsalari.wrench.service.ServiceService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.time.Instant;
-
-@SpringBootApplication
+@EnableWebMvc
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class WrenchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WrenchApplication.class, args);
     }
-
-    @Bean
-    CommandLineRunner commandLineRunner(ServiceConfig serviceConfig,
-                                        ExpertService expertService
-                                        ) {
-        return args -> {
-//			serviceConfig.start();
-//            Expert expert = new Expert(
-//                    1L,
-//                    "Ali",
-//                    "Mirsalari",
-//                    "Ali@gmail.com",
-//                    "Fr3$#B1gD0g$RanTh3H1ll$F@st!",
-//                    Instant.now(),
-//                    "src/main/resources/images/Avatar.jpg");
-//            expertService.save(expert);
-
-//            expertService.retrieveAndSavePhotoToFile(expertService.findById(1L).get(), "src/main/resources/images/new/Avatar.jpg");
-        };
-    }
-
 }
