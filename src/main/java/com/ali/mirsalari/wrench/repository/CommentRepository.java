31b3ece231107e4386397e86abf7ac0cfd78509d
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-26 23:54:39 +0330

    Update CommentRepository.java

--- src/main/java/com/ali/mirsalari/wrench/repository/CommentRepository.java
+++ src/main/java/com/ali/mirsalari/wrench/repository/CommentRepository.java
@@ -4,7 +4,11 @@ import com.ali.mirsalari.wrench.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository
         extends JpaRepository<Comment,Long> {
+
+    List<Comment> findCommentsByExpertId(Long expertId);
 }
