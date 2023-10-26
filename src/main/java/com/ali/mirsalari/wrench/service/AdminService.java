8a36aba142b9817532ec6f797d88abfbf72a3328
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-26 23:49:24 +0330

    Update AdminService.java

--- src/main/java/com/ali/mirsalari/wrench/service/AdminService.java
+++ src/main/java/com/ali/mirsalari/wrench/service/AdminService.java
@@ -2,11 +2,38 @@ package com.ali.mirsalari.wrench.service;
 
 import com.ali.mirsalari.wrench.entity.Admin;
 import com.ali.mirsalari.wrench.service.base.CrudService;
-import com.ali.mirsalari.wrench.service.base.UserService;
+import com.ali.mirsalari.wrench.service.base.BaseUserService;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface AdminService
-        extends CrudService<Admin, Long>,
-        UserService<Admin, String> {
+        extends BaseUserService<Admin, String> {
+    @Transactional
+    Admin save(String firstName,
+               String lastName,
+               String email,
+               String password);
+
+    @Transactional
+    Admin update(Long id,
+                 String firstName,
+                 String lastName,
+                 String email,
+                 String password);
+
+    @Transactional
+    Admin uncheckedUpdate(Admin admin);
+
+    @Transactional
+    void remove(Long id);
+
+    @Transactional
+    Optional<Admin> findById(Long id);
+
+    @Transactional
+    List<Admin> findAll();
 }
