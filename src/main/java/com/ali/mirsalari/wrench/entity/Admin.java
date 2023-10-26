5deafa0654aefd2faebeeb392ce41ecf734c2493
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-26 23:48:29 +0330

    Update Admin.java

--- src/main/java/com/ali/mirsalari/wrench/entity/Admin.java
+++ src/main/java/com/ali/mirsalari/wrench/entity/Admin.java
@@ -1,18 +1,25 @@
 package com.ali.mirsalari.wrench.entity;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
-import java.time.Instant;
+import java.util.Objects;
 
-@EqualsAndHashCode(callSuper = true)
-@Data
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
 @Entity
 public class Admin extends User{
-    public Admin(String firstName, String lastName, String email, String password, Long credit, Instant registerTime) {
-        super(firstName, lastName, email, password, credit, registerTime);
+    public Admin(String firstName, String lastName, String email, String password) {
+        super(firstName, lastName, email, password);
     }
+
+    public Admin(Long id, String firstName, String lastName, @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$") String email, @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$!%^&+=])(?=\\S+$).{8,}$") String password) {
+        super(id, firstName, lastName, email, password);
+    }
+
 }
