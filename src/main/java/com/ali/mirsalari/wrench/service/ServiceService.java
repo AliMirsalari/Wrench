3bb0d50a584bd267a179d62807740df26474ff58
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-27 20:57:13 +0330

    Add ServiceService.java

--- src/main/java/com/ali/mirsalari/wrench/service/ServiceService.java
+++ src/main/java/com/ali/mirsalari/wrench/service/ServiceService.java
@@ -1,21 +1,35 @@
 package com.ali.mirsalari.wrench.service;
 
 import com.ali.mirsalari.wrench.entity.Service;
-import com.ali.mirsalari.wrench.exception.NotFoundException;
-import com.ali.mirsalari.wrench.exception.ServiceExistException;
-import com.ali.mirsalari.wrench.service.base.CrudService;
 import jakarta.transaction.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @org.springframework.stereotype.Service
-public interface ServiceService
-        extends CrudService<Service, Long> {
+public interface ServiceService {
+
+    Service saveSubservice(String name, Long basePrice, String description, Long serviceParentId);
+
+    Service saveServices(String name);
+
+
+    Service updateSubservice(Long id, String name, Long basePrice, String description, Long serviceParentId);
+
+    Service updateService(Long id, String name);
+
+    Service uncheckedUpdate(Service service);
+
+    void remove(Long id);
+
+    Optional<Service> findById(Long id);
+
+    List<Service> findAll();
+
     @Transactional
     List<Service> findAllServices ();
     @Transactional
-    List<Service> findAllSubServices ();
+    List<Service> findAllSubservices ();
     @Transactional
     Optional<Service> findByName(String name);
 
