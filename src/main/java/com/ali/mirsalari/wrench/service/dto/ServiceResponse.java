1701f4f6ca18b44b6387daf14557777bdd144a57
Ali Mirsalari <AliMirsalari@hotmail.com>
2023-10-28 01:46:30 +0330

    Add ServiceResponse.java

---
+++ src/main/java/com/ali/mirsalari/wrench/service/dto/ServiceResponse.java
@@ -0,0 +1,11 @@
+package com.ali.mirsalari.wrench.service.dto;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link com.ali.mirsalari.wrench.entity.Service}
+ */
+public record ServiceResponse(Long id, String name, Long basePrice, String description, Long serviceParentId,
+                              String serviceParentName, Long serviceParentBasePrice,
+                              String serviceParentDescription) implements Serializable {
+}
